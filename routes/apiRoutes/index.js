78be571a1e2f89f4176faa1bf74dbcaf4dd76249
Marykate Smith <Mkayy@Marykates-MacBook-Pro.local>
2021-06-27 16:57:07 -0400

    trying to pass data into db.json file

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const router = require('express').Router();
+const notesRoute = require('../apiRoutes/notesRoute');
 
 // need to get router for api
+router.use(notesRoute);
 
 module.exports = router;
