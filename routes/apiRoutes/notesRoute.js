1828a2d2358337963975af157c212939511c7094
Marykate Smith <Mkayy@Marykates-MacBook-Pro.local>
2021-06-27 17:14:40 -0400

    trying to connect to API routes

--- routes/apiRoutes/notesRoute.js
+++ routes/apiRoutes/notesRoute.js
@@ -1,2 +1,4 @@
-// const router = require('express').Router();
-// const { createNewNote } = 
+//  const router = require('express').Router();
+//  const { createNewNote } = require('../../lib/notes');
+
+// do i need two files in apiroutes or can this all be one since only getting and posting one thing?
